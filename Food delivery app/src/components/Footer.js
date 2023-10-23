6425b1e11d8614c11f737b12e94c10ad09685cb6
Abhishek Magdum <ammagdum76@gmail.com>
2023-10-23 21:17:42 +0530

    Update Footer.js

--- Food delivery app/src/components/Footer.js
+++ Food delivery app/src/components/Footer.js
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <div className=" text-center text-sm font-medium w-full  h-9 flex justify-center items-center bg-red-600  mt-8">
-      <h1 className="text-sm font-medium text-slate-200">© 2023 made by Abhishek </h1>
+      <h1 className="text-sm font-medium text-slate-50">© 2023 Food Delight</h1>
     </div>
   );
 };
